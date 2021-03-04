07b4d5375b9c6baf744e3dddc8e80bed13a3af73
DankenStinks <59336456+DankenStinks@users.noreply.github.com>
2021-03-05 10:07:35 +1100

    Added Basic Player Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public string Left;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void Update()
+    {
+        //physcis
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("Left", true);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("Left", false);
+        }
+    }
+}
